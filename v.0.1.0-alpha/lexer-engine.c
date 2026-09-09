c3098566194eec56c018d529f64b5ac0a4cfadbe
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-09-09 13:38:54 +1100

    .

--- v.0.1.0-alpha/lexer-engine.c
+++ v.0.1.0-alpha/lexer-engine.c
@@ -3,14 +3,23 @@
 
 #include "function-declaration.txt"
 
+#define ln_() putchar('\n')
+
 int main(int argc, char *argv[])
 {
     setlocale(0, "");
     printf(" Количество аргументов: %d", argc);
     for (int i = 0; i < argc; i++) printf("\n Имя аргумента %d: %s", i, argv[i]);
+    ln_();
+    
+    FILE *file = fopen(argv[1], "rb");
+    if (file != NULL)
+    {
+        printf(" Не удалось открыть файл <%s> на чтение", argv[1]);
+    }
+    fclose(file);
     return 0;
 }
-
 void compilation()
 {
     // ... //
