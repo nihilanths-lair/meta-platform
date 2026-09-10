282e82677a5ba53c24cd86957ee165e23622c9ed
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-09-11 01:26:07 +1100

    .

--- v.0.1.0-beta/lexer-engine.c
+++ v.0.1.0-beta/lexer-engine.c
@@ -36,6 +36,8 @@ int main(int argc, char *argv[])
         size_t text_size = fread(text, sizeof (char), file_size, file);
         ln_(); printf(" text_size: %llu", text_size);
         ln_(); for (int i = 0; i < file_size; i++) putchar(text[i]);
+        ln_(); for (int i = 0; i < file_size; i++) printf(" %2c", text[i]);
+        ln_(); for (int i = 0; i < file_size; i++) printf(" %02X", text[i]);
         execution();
     }
     case 2: // Сканирование файла, без полной загрузки в память
