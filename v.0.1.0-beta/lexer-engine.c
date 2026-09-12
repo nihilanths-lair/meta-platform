d4ec8d97005bdc58ca8ef6b0d98d8626ed514f25
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-09-13 09:33:24 +1100

    .

--- v.0.1.0-beta/lexer-engine.c
+++ v.0.1.0-beta/lexer-engine.c
@@ -10,9 +10,18 @@
 #define x_ {
 #define _x }
 
+#define anonymous_scope_(arg) { arg }
+
 int main(int argc, char *argv[])
 {
     setlocale(0, "");
+    // Передаем весь блок инструкций прямо внутрь макроса
+    anonymous_scope_(
+        int x = 5;
+        int y = 10;
+        printf("Сумма: %d\n", x + y);
+    )
+    // Здесь переменные x и y больше не существуют
     printf(" Количество аргументов: %d", argc);
     for (int i = 0; i < argc; i++)
     {
@@ -40,11 +49,11 @@ int main(int argc, char *argv[])
         ln_(1);
         printf(" text_size: %llu", text_size);
         ln_(1);
-        for (int i = 0, string_length = 0, max_string_length = 0; i < file_size; i++)
+        for (int current_pos = 0, string_length = 0, max_string_length = 0; current_pos < file_size; )
         {
-            if (text[i] == '\r' && text[i+1] == '\n') // Если среда Windows
+            if (text[current_pos] == '\r' && text[current_pos+1] == '\n') // Если среда Windows
             {
-                i++;
+                current_pos += 2;
                 string_length += 2;
                 if (string_length > max_string_length)
                 {
@@ -53,7 +62,11 @@ int main(int argc, char *argv[])
                     printf("\n max_string_length: %u", max_string_length);
                 }
             }
-            string_length++;
+            else
+            {
+                current_pos++;
+                string_length++;
+            }
         }
         ln_(1);
         for (int i = 0, j; i < file_size; i++)
