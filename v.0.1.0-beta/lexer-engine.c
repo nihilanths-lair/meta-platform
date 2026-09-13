46b74d0261dc8618676b7a00a81ce10ad859ae72
Gleb Ovdeenko <gleb.ovdeenko@gmail.com>
2026-09-13 12:47:58 +1100

    .

--- v.0.1.0-beta/lexer-engine.c
+++ v.0.1.0-beta/lexer-engine.c
@@ -12,9 +12,31 @@
 
 #define anonymous_scope_(arg) { arg }
 
+#define fch_(arg) fch[arg]
+
 int main(int argc, char *argv[])
 {
     setlocale(0, "");
+    const unsigned char fch[256] =
+    {
+        // 0x00 - 0x1F: 32 пустых управляющих символа (заменены пробелами)
+        "                                "
+
+        // 0x20 - 0x7F: Пробел, знаки, латиница. Опасные ", \, ' пишем через HEX
+        " !\"#$%&'\x27()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\x5C]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+
+        // 0x80 - 0xBF: 64 байта расширенной CP1251 (заменили пробелами, кроме ё/Ё)
+        // Чтобы буквы 'ё' и 'Ё' не съехали из-за UTF-8, пишем их строго в CP1251 кодах:
+        // \xA8 = 'Ё' (индекс 168), \xB8 = 'ё' (индекс 184)
+        "                                " // 0x80 - 0x9F (32 пробела)
+        "        \xA8       "               // 0xA0 - 0xAF (0xA8 на своем месте)
+        "        \xB8       "               // 0xB0 - 0xBF (0xB8 на своем месте)
+
+        // 0xC0 - 0xFF: Вся кириллица А-Я и а-я (64 байта)
+        // Чтобы она занимала по 1 байту на знак, ваш файл .c ДОЛЖЕН быть сохранен в кодировке CP1251!
+        "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+        "абвгдежзийклмнопрстуфхцчшщъыьэюя"//;
+    };
     // Передаем весь блок инструкций прямо внутрь макроса
     anonymous_scope_(
         int x = 5;
@@ -22,6 +44,27 @@ int main(int argc, char *argv[])
         printf("Сумма: %d\n", x + y);
     )
     // Здесь переменные x и y больше не существуют
+    /*
+    for (int i = 0; i < 256; i++) ascii[i] = i;
+    ascii['\0'] = ' '; // 0
+    for (int i = 1; i <= 6; i++) ascii[i] = ' '; // Глушим нижний диапазон 1-6
+    ascii['\a'] = ' '; // 7
+    ascii['\b'] = ' '; // 8
+    ascii['\t'] = ' '; // 9
+    ascii['\n'] = ' '; // 10
+    ascii['\v'] = ' '; // 11
+    ascii['\f'] = ' '; // 12
+    ascii['\r'] = ' '; // 13
+    for (int i = 14; i <= 26; i++) ascii[i] = ' '; // Глушим нижний диапазон 14-26
+    ascii['\e'] = ' '; // 27
+    for (int i = 28; i <= 31; i++) ascii[i] = ' '; // Глушим нижний диапазон 28-31
+    for (int i = 0x7F; i <= 159; i++) ascii[i] = ' '; // Глушим 127 и верхний управляющий диапазон C1 (128 - 159)
+    for (int i = 161; i <= 167; i++) ascii[i] = ' '; // Глушим символы-призраки
+    for (int i = 169; i <= 183; i++) ascii[i] = ' ';
+    for (int i = 185; i <= 191; i++) ascii[i] = ' ';
+    for (int i = 0; i < 256; i++) printf("%c", ascii[i]);
+    ln_(1);
+    */
     printf(" Количество аргументов: %d", argc);
     for (int i = 0; i < argc; i++)
     {
@@ -72,7 +115,7 @@ int main(int argc, char *argv[])
             // === ИЗОЛИРОВАННЫЙ БЛОК ХВОСТА (Выполняется строго 1 раз за жизнь программы) ===
             // Сюда мы гарантированно приземляемся, когда цикл закрылся сам.
             // Если файл закончился без \r\n, в string_length останется «хвост» последней строки.
-            if (string_length > 0) { if (string_length > max_string_length) { max_string_length = string_length; } }
+            if (string_length > 0) { if (string_length > max_string_length) max_string_length = string_length; }
             printf("\n checkpoint: max_string_length (only print chars): %u\n", max_string_length);
             ln_(1);
             int j;
@@ -90,7 +133,7 @@ int main(int argc, char *argv[])
                         //string_length += 2;
                         break;
                     }
-                    printf("%c", text[current_pos]);
+                    printf("%c", fch_(text[current_pos]));
                     current_pos++;
                     string_length++;
                 }
